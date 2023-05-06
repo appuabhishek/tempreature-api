0f9c88be430988881d11940b707e6129c7cdb09c
ABHISHEK S 1ga19cs003 <appuabhishek525@gmail.com>
2023-05-06 19:58:04 +0530

    images added

--- apiTemp.py
+++ apiTemp.py
@@ -1,21 +1,65 @@
 import sys
-import Adafruit_DHT as dht
-import urllib2
 
-myAPI = 'IUKV2ZRBQW9MV407Q' 
+import RPi.GPIO as GPIO
+
+import dht11
+
+import time
+
+import datetime
+
+import requests
+
+
+
+myAPI = 'PL0FQK5P82YX909X' 
+
 ThingsURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI 
 
-def DHT22_data():
-	humidity, temperature = dht.read_retry(dht.DHT22, 23) 
-	return humi, temp
 
-humidity, temp = DHT22_data()
-if isinstance(humidity, float) and isinstance(temp, float):
-	humi = '%.2f' % humidity 					   
-	temp = '%.2f' % temp
+
+# initialize GPIO
+
+GPIO.setwarnings(True)
+
+GPIO.setmode(GPIO.BCM)
+
+
+
+# read data using pin 14
+
+instance = dht11.DHT11(pin=4)
+
+try:
+
+    while True:
+
+        result = instance.read()
+
+        if result.is_valid():
+
+            print("Last valid input: " + str(datetime.datetime.now()))
+
+            print("Temperature: %-3.1f C" % result.temperature)
+
+            print("Humidity: %-3.1f %%" % result.humidity)
+
+            r = requests.post(ThingsURL + '&field1=%s&field2=%s' % (result.temperature, result.humidity))
+
+            print(r.text)
+
+        time.sleep(6)
+
+
+
+except KeyboardInterrupt:
+
+    print("Cleanup")
+
+    GPIO.cleanup()
+
+
+
 			
-	coms = urllib2.urlopen(ThingsURL + '&field1=%s&field2=%s' % (temp, humi))
-	print coms.read()
-	coms.close()
-else:
-	print 'Error'
+
+	
